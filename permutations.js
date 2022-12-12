846ae16502c1b2bef8b68063914582775d054615
svakam <vikramakam@hotmail.com>
2022-12-11 18:07:37 -0800

    added permutations

--- permutations.js
+++ permutations.js
@@ -1,12 +1,41 @@
 function permute(nums) {
     let output = []
+    let used = new Set()
 
-    
+    let recurse = function(idx, currPermutation) {
+        if (idx === nums.length) {
+            output.push([...currPermutation])
+            return
+        }
+
+        for (const num of nums) {
+            if (!used.has(num)) {
+                used.add(num)
+                currPermutation.push(num)
+                recurse(idx + 1, currPermutation)
+                used.delete(num)
+                currPermutation.pop()
+            }
+        }
+    }
+
+    recurse(0, [])
+
+    return output
+
+    // 3 spots for available numbers
+    // at every spot, can either include a given number or not
+    // once seat occupied, remove number from available numbers and recurse on next
+    // base case: once all seats are occupied, add to output
 }
 
-console.log(permute([]), [])
-console.log(permute([1]), [[1]])
-console.log(permute([1,2]), [[1,2],[2,1]])
-console.log(permute([1,2,3]), [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
-console.log(permute([1,2,3,4]), [[1,2,3,4],[1,2,4,3],[1,3,2,4],[1,3,4,2],[1,4,2,3],[1,4,3,2],[2,1,3,4],[2,1,4,3],[2,3,1,4],[2,3,4,1],
-[3,1,2,4],[3,1,4,2],[3,2,1,4],[3,2,4,1],[4,1,2,3],[4,1,3,2],[4,2,1,3],[4,2,3,1]])
+console.log(permute([]))
+console.log(permute([1]))
+console.log(permute([1,2]))
+console.log(permute([1,2,3]))
+console.log(permute([1,2,3,4]))
+
+
+
+// time complexity: O(2^n), n = number of elements in input arr
+// space: O()
