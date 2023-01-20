609935bc31dccc29dee1cc069c10f2c19e1209f4
svakam <vikramakam@hotmail.com>
2023-01-20 15:27:58 -0800

    find min max recursive

---
+++ Easy/Complete/minMax.js
@@ -0,0 +1,47 @@
+function findMinIndex(arr) {
+    function getMin(currIndex) {
+        if (currIndex === arr.length - 1) { // base case
+            return currIndex
+        }
+
+        const smallestIndex = getMin(currIndex + 1) // recursion
+
+        if (arr[smallestIndex] < arr[currIndex]) {
+            return smallestIndex
+        }
+        if (arr[smallestIndex] > arr[currIndex]) {
+            return currIndex
+        }
+        else {
+            return Math.min(smallestIndex, currIndex) // get first occurrence
+        }
+    }
+
+    return getMin(0)
+}
+  
+function findMaxIndex(arr) {
+    function getMax(currIndex) {
+        if (currIndex === arr.length - 1) { // base case
+            return currIndex
+        }
+
+        const largestIndex = getMax(currIndex + 1)
+
+        if (arr[currIndex] > arr[largestIndex]) {
+            return currIndex
+        }
+        if (arr[currIndex] < arr[largestIndex]) {
+            return largestIndex
+        }
+        else {
+            return Math.min(largestIndex, currIndex) // get first occurrence
+        }
+    }
+
+    return getMax(0)
+}
+
+let arr = [8,16,-3,2,5,100, 9]
+console.log(findMinIndex(arr), 2)
+console.log(findMaxIndex(arr), 5)
