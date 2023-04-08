614077f5673cc80fe036e2b6910898f1620edf5d
svakam <vikramakam@hotmail.com>
2023-04-07 18:35:37 -0700

    power of k

--- Easy/Incomplete/isPowerOfK.js
+++ Easy/Incomplete/isPowerOfK.js
@@ -1,1 +1,26 @@
-// determine if a number n is a power of some other number k; n = 9 and k = 3, return true (3^2 = 9)
+// determine if a number n is a power of some other number k without using built in library; n = 27 and k = 3, return true (3^3 = 27)
+
+function isPowerOfK(n, k) {
+    if (k === 1 && n === 1) return true
+    if (k === 1 && n > 1) return false
+    while (n > 1 && n % k === 0) {
+        n /= k
+    }
+    return n === 1
+}
+
+// 27 > 1 && 27 % 3 === 0
+// n = 9
+// 9 > 1 && 9 % 3 === 0
+// n = 3
+// 3 > 1 && 3 % 3 === 0
+// n = 1
+// 
+
+console.log(isPowerOfK(27, 3), true)
+console.log(isPowerOfK(4, 2), true)
+console.log(isPowerOfK(5, 2), false)
+console.log(isPowerOfK(10, 2), false)
+console.log(isPowerOfK(1, 1), true)
+console.log(isPowerOfK(10, 1), false)
+console.log(isPowerOfK(1, 1), false)
