97a9b222c26f60d4eed30b70a45925779054f620
svakam <vikramakam@hotmail.com>
2023-05-26 19:26:48 -0700

    at least n occurrences

--- Medium/Complete/atLeastNOccurrencesSubstr.js
+++ Medium/Complete/atLeastNOccurrencesSubstr.js
@@ -1,6 +1,6 @@
 function count(str, substr, reqOcc) {
 
-    let recurse = function(str, substr) {
+    let helper = function(str, substr) {
         if (str.length < substr.length) return 0
 
         if (str.slice(0, substr.length) === substr) {
@@ -10,10 +10,10 @@ function count(str, substr, reqOcc) {
         return recurse(str.slice(1), substr)
     }
 
-    return recurse(str, substr) >= reqOcc
+    return helper(str, substr) >= reqOcc
 }
 
-console.log(count("catcowcat", "cat", 1), true)
+console.log(count("catcowcat", "cat", 1), true) // catcowcat 1 cowcat owcat wcat cat 2 
 console.log(count("catcowcat", "cat", 2), true)
 console.log(count("catcowcat", "cat", 3), false)
 console.log(count("catcowcat", "cow", 2), false)
@@ -21,3 +21,7 @@ console.log(count("catcowcat", "cow", 1), true)
 console.log(count("at", "cat", 0), true)
 console.log(count("at", "cat", 1), false)
 
+// substring problems: 
+// recursion
+// pass in string and substring
+// each call slices by substr length to check if it's target substr, if so slice by substr length, else slice by 1 to check incrementally
