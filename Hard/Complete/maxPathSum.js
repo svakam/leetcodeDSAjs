0166c5dc4e4fc19db649fc3a3f085eac5f3618d6
svakam <vikramakam@hotmail.com>
2023-06-08 23:41:23 -0700

    prog

--- Hard/Complete/maxPathSum.js
+++ Hard/Complete/maxPathSum.js
@@ -1,13 +1,22 @@
+class TNode {
+    constructor(val, left = null, right = null) {
+        this.val = val,
+        this.left = left,
+        this.right = right
+    }
+}
+
 function solution(root) {
     let max = -Infinity // global max tracker
     
     function helper(node) {
         // if null, no sum here; return 0
         if (!node) return 0
-        
+        console.log(node.val)
         // get sums from left and right sides
         let leftSum = helper(node.left)
         let rightSum = helper(node.right)
+        console.log(leftSum, rightSum)
         // another possible sum is including curr node val
         let currLeftSum = leftSum + node.value
         let currRightSum = rightSum + node.value
@@ -24,3 +33,8 @@ function solution(root) {
     
     return max
 }
+
+let t1 = new TNode(1, new TNode(2), new TNode(3))
+console.log(solution(t1), 6)
+let t2 = new TNode(-10, new TNode(9), new TNode(20, new TNode(15), new TNode(7)))
+// console.log(solution(t2), 42)
