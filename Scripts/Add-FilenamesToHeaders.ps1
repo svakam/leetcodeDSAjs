4a5d5c9a5079a3031ead0a31d614e3eabb62bf16
svakam <vikramakam@hotmail.com>
2023-07-06 14:00:21 -0700

    refactored modularization, now appending to LastModified.md

--- Scripts/Add-FilenamesToHeaders.ps1
+++ Scripts/Add-FilenamesToHeaders.ps1
@@ -1,4 +1,7 @@
-﻿# class called File_ModDate with properties FileName (name of file + extension) and LastModified (file's last-modified date on the system)
+﻿# dot-source script that re-writes the LastModified file with the updated dictionary of new/updated files with last modified dates
+. .\Scripts\ReWrite-ToLastModified.ps1
+
+# class called File_ModDate with properties FileName (name of file + extension) and LastModified (file's last-modified date on the system)
 class File_ModDate {
     [string]$FileName
     [DateTime]$LastModified
@@ -17,16 +20,11 @@ function Add-FilenamesToHeaders {
 
     Write-Host "Adding file names to headers."
     Write-Host "Repo path: $repoPath"
-    Write-Host "Test after modifying module"
     Write-Host "Last modified file path: $filePath"
 
-    # add existing headers to a hashtable
     $lastModTable = [ordered]@{}
-    $bl = Get-Date
-    $obja = [File_ModDate]::new(
-        "test",
-        $bl
-    )
+
+    # add existing headers to a hashtable
     (Get-Content -Path $filePath) |
         ForEach-Object {
             if ($_ -like "##*") {
@@ -35,25 +33,19 @@ function Add-FilenamesToHeaders {
             }
         }
 
-    $test = (Get-Date -Month 6 -Day 4 -Year 2023 -Hour 0 -Minute 0 -Second 0 -Millisecond 0)
-    #foreach ($key in $($lastModTable.keys)) {
-    #    $list = ($lastModTable[$key])
-    #    $list.Add($obja)
-    #    $lastModTable[$key] = $list
-    #}
-    $lastModTable
-
     # recurse through all valid files
         # for each file, get first sunday of its week
-        # if this sunday exists in the table, add this file and its last modified date as an object to value collection
-    (Get-ChildItem -Path $repoPath -Exclude "*.git*","*.md","*.txt","Node*","*.ps1") |
+        # if this sunday exists in the table, add this file and its last modified date as File_ModDate obj to its Sunday key list
+    $count = 0
+    (Get-ChildItem -Path $repoPath -Exclude "*.git*","*.md","*.txt","Node*","Scripts*","*.ps1","*.psm1","*.json") | # exclude irrelevant file types
         Get-ChildItem -Recurse -File | ForEach-Object {
-            $lastWriteTime = $_.LastWriteTime
-            $firstSunday = $lastWriteTime.AddDays(-($lastWriteTime.DayOfWeek)) 
-            $firstSunZeroed = Get-Date -Date $firstSunday.Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0
-            if (!$lastModTable.Contains($firstSunZeroed)) {
+            $count++
+            $lastWriteTime = $_.LastWriteTime # get last write time of this file
+            $firstSunday = $lastWriteTime.AddDays(-($lastWriteTime.DayOfWeek)) # get the first sunday Date of this object
+            $firstSunZeroed = Get-Date -Date $firstSunday.Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0 # zero-out this sunday Date
+            if (!$lastModTable.Contains($firstSunZeroed)) { # if the table doesn't contain this sunday, add it as a key with a value of List<File_ModDate>
                 $lastModTable.Add($firstSunZeroed, (New-Object System.Collections.Generic.List[File_ModDate]))
-            } 
+            }
             else {
                 # create object
                 $fileModDateObj = [File_ModDate]::new(
@@ -65,6 +57,12 @@ function Add-FilenamesToHeaders {
                 $lastModTable[$firstSunZeroed] = $list
             }
         }
+    Write-Host "Number of files processed: $count"
     $lastModTable
-    Write-Output "last mod table"
+    Write-Output "last mod table before ordering"
+    Write-Output ""
+
+    $sortedPairs = $lastModTable.GetEnumerator() | Sort-Object -Property Name -Descending
+    ReWrite-ToLastModified -FilePath $filePath -SortedTable $sortedPairs
+    
 }
---
+++ Scripts/ReWrite-ToLastModified.ps1
@@ -0,0 +1,28 @@
+﻿function ReWrite-ToLastModified {
+    param (
+        $filePath,
+        $sortedTable
+    )
+
+    Write-Host "Rewriting last modified."
+
+    Set-Content -Path $filePath "# Last Modified`n"
+    
+    foreach ($kvObj in $sortedTable) {
+        $kvObj | ForEach-Object {
+            if ($_.Value.Length -gt 0) {
+                $sunday = $_.Key
+                $file_modDateListThisWeek = $_.Value
+                $file_modDateListThisWeek
+                (Add-Content -Path $filePath "## Week of Sunday, $($sunday.Month)/$($sunday.Day)/$($sunday.Year)`n") | Wait-Process
+                $file_modDateListThisWeek | ForEach-Object {
+                    (Add-Content -Path $filePath "$($_.FileName), last modified at $($_.LastModified)`n") | Wait-Process
+                }
+            } else {
+                Write-Host "$($_.Key) has no files in this week, skip appending"
+            }
+        }
+        # add header
+        # add file names under this header if any
+    }
+}
