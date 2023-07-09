2485c61ab9b6ac4bbdcc290f9043b7c10abc3fd4
svakam <vikramakam@hotmail.com>
2023-07-08 19:40:23 -0700

    few more changes to readme script

--- Scripts/Overwrite-Readme.ps1
+++ Scripts/Overwrite-Readme.ps1
@@ -9,6 +9,7 @@
     $filePath
 
     Set-Content -Path $filePath "# Leetcode JS`n`nMy repository of Leetcode and general algo problems in JavaScript.`n"
+    Add-Content -Path $filePath "(last auto-update $((Get-Date).DateTime))`n"
     Add-Content -Path $filePath "Files by last modified date, most recent to least: [LastModified.md](LastModified.md)`n"
     Add-Content -Path $filePath "Change log (WIP): [ChangeLog.md](ChangeLog.md)`n`n## Categories`n"
 
@@ -16,9 +17,9 @@
     foreach ($category in $categories) {
         $catList = $categoryTable[$category]
 
-        (Add-Content -Path $filePath "### $($category)`n`n") | Wait-Process
+        (Add-Content -Path $filePath "### $($category)`n") | Wait-Process
         foreach ($file in $catList) {
-            (Add-Content -Path $filePath "[$($file.FileName)]($($file.RelPath))`n`n") | Wait-Process
+            (Add-Content -Path $filePath "[$($file.FileName)]($($file.RelPath))`n") | Wait-Process
         }
     }
 
